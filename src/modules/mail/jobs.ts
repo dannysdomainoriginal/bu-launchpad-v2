e73a41f7df1b0caf65dec0f0b6439634ac3ddee8
Charles Daniel <dannysdomainoriginal@gmail.com>
2026-07-07 09:00:43 +0100

    fix: cleared old dependencies and folders

--- src/modules/mail/jobs.ts
+++
@@ -1,5 +0,0 @@
-export const EMAIL_JOB = {
-  FEEDBACK: "feedback-email",
-  COLLABORATION: "collaboration-email",
-  WELCOME: "welcome-email",
-} as const;
--- src/modules/mail/templates/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as feedbackEmailTemplate } from "./feedbackEmailTemplate";
-export { default as collaborationEmailTemplate } from "./collaborationEmailTemplate";
