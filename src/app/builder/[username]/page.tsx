237ca3e28645781ff2bbfc6f2ab1ff8ab4e5457a
Charles Daniel <dannysdomainoriginal@gmail.com>
2026-07-12 13:55:59 +0100

    chore: removing builder profile page to pass vercel deployment check

--- src/app/builder/[username]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use cache";
-
-type Props = {
-  params: Promise<{ username: string }>;
-};
-
-export default function BuilderProfilePage({ params }: Props) {
-  return <div>BuilderProfilePage</div>;
-}
